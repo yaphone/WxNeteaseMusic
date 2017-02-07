a20b429d4e76ae216e099c5edca87827450deccb
yaphone <zhouyafeng2013@gmail.com>
2017-02-07 23:11:52 +0800

    日常开发

--- test3.py
+++ test3.py
@@ -5,7 +5,7 @@ from threading import Thread
 from Queue import Queue
 
 def enqueue_output(out, queue):
-    for line in iter(out.readline, b""):
+    for line in iter(out.read, b""):
         queue.put(line)
     out.close()
 ON_POSIX = "posix" in sys.builtin_module_names
@@ -16,8 +16,9 @@ t = Thread(target=enqueue_output, args=(p.stdout, q))
 t.daemon = True
 t.start()
 
+#print p.stdout.read()
 try:
-    line = q.get_nowait()
+    line = q.get(timeout=4000000)
 except:
     print "***********"
 else:
